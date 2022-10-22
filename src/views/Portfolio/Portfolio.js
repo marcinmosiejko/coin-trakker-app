1aeb3efafbdfb450fa898732fd8c8ea92075b75e
Marcin <marcinmosiejko@gmail.com>
2022-10-22 15:13:21 +0200

    Add pagination to portfolio

--- src/views/Portfolio/Portfolio.js
+++ src/views/Portfolio/Portfolio.js
@@ -1,9 +1,10 @@
 import React, { useState, useRef } from 'react';
 import { Wrapper } from './Portfolio.styles';
 import Table from 'components/organisms/Table/Table';
-import TableBody from 'components/organisms/TableBody/TableBody';
 import PortfolioTableHead from 'components/molecules/PortfolioTableHead/PortfolioTableHead';
+import TableBody from 'components/organisms/TableBody/TableBody';
 import { usePages } from 'hooks/usePages';
+import Pagination from 'components/molecules/Pagination/Pagination';
 
 const Portfolio = () => {
   const {
@@ -37,6 +38,14 @@ const Portfolio = () => {
             )}
           </Table>
         </div>
+
+        <Pagination
+          currentPage={currentPage}
+          lastPage={lastPage}
+          handlePageChange={({ selected }) =>
+            handlePageChange(selected, tableRef)
+          }
+        />
       </>
     </Wrapper>
   );
