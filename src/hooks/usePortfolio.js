a25c87ea5c9aec0ee0a22652a1afacfa65021e76
Marcin <marcinmosiejko@gmail.com>
2022-10-24 21:28:29 +0200

    Add usePortfolio hook that manages portfolio state

---
+++ src/hooks/usePortfolio.js
@@ -0,0 +1,84 @@
+import { useEffect, useState } from 'react';
+import { useLcwCoinsData } from 'hooks/useLcwCoinsData';
+import { PER_PAGE_LIMIT_DEFAULT } from 'config';
+import { usePages } from 'hooks/usePages';
+import { getFromLocalStorage, saveToLocalStorage } from 'helpers/general';
+
+const usersPortfolio = getFromLocalStorage('portfolio');
+
+export const usePortfolio = () => {
+  const { coinsData, findCoins } = useLcwCoinsData();
+  const [portfolioData, setPortfolioData] = useState(usersPortfolio || null);
+  const [portfolioCoinsList, setPortfolioCoinsList] = useState(null);
+  const [portfolioCurPageCoinsList, setPortfolioCurPageCoinsList] =
+    useState(null);
+  const [portfolioSummary, setPortfolioSummary] = useState({ totalValue: 0 });
+  const { currentPage, lastPage, handlePageChange } = usePages();
+  const perPageLimit = PER_PAGE_LIMIT_DEFAULT;
+
+  const handleSetPortfolioData = (coin) => {
+    setPortfolioData((prevState) => [...prevState, coin]);
+  };
+
+  const findPortfolioCoins = (queryString) => {
+    // Filters matching coins from coinsData so there are no coins that are already in portfolio -> use won't be able to add same coin more then once
+    return findCoins(queryString).filter(
+      (coin) => !portfolioData.find(({ code }) => code === coin.code)
+    );
+  };
+
+  useEffect(() => {
+    saveToLocalStorage('portfolio', portfolioData);
+  }, [portfolioData]);
+
+  useEffect(() => {
+    if (!coinsData) return;
+
+    const portfolioCoinsList = portfolioData
+      .map(({ code: portfolioCoinCode, quantity }) => {
+        const portfolioCoin = coinsData.find(
+          (coin) => coin.code === portfolioCoinCode
+        );
+        // In case no coins with specified code were found
+        if (!portfolioCoin) return undefined;
+
+        const { code, name, rate, webp64, delta } = portfolioCoin;
+        const value = rate * quantity;
+        return { code, name, rate, webp64, delta, quantity, value };
+      })
+      // In case no coins with specified code were found
+      .filter((item) => item !== undefined);
+
+    setPortfolioCoinsList(portfolioCoinsList);
+
+    const totalValue = portfolioCoinsList.reduce(
+      (acc, coin) => acc + coin.quantity * coin.rate,
+      0
+    );
+    setPortfolioSummary((prevState) => ({ ...prevState, totalValue }));
+  }, [coinsData, portfolioData]);
+
+  useEffect(() => {
+    if (!portfolioCoinsList) return;
+
+    const pageStartIndex = currentPage * perPageLimit;
+    const pageEndIndex = (currentPage + 1) * perPageLimit;
+
+    const curPageCoinsList = portfolioCoinsList.slice(
+      pageStartIndex,
+      pageEndIndex
+    );
+
+    setPortfolioCurPageCoinsList(curPageCoinsList);
+  }, [portfolioCoinsList, currentPage, perPageLimit]);
+
+  return {
+    handleSetPortfolioData,
+    findPortfolioCoins,
+    portfolioCurPageCoinsList,
+    portfolioSummary,
+    currentPage,
+    lastPage,
+    handlePageChange,
+  };
+};
--- src/views/Portfolio/Portfolio.js
+++ src/views/Portfolio/Portfolio.js
@@ -5,84 +5,25 @@ import PortfolioTableHead from 'components/molecules/PortfolioTableHead/Portfoli
 import TableBody from 'components/organisms/TableBody/TableBody';
 import Modal from 'components/organisms/Modal/Modal';
 import AddCoin from 'components/molecules/AddCoin/AddCoin';
-import { usePages } from 'hooks/usePages';
 import Pagination from 'components/molecules/Pagination/Pagination';
 import { TableOptionsWrapper } from 'components/atoms/TableOptionsWrapper/TableOptionsWrapper';
 import Button from 'components/atoms/Button/Button';
 import useModal from 'hooks/useModal';
-import { useLcwCoinsData } from 'hooks/useLcwCoinsData';
-import { PER_PAGE_LIMIT_DEFAULT } from 'config';
-import { getFromLocalStorage, saveToLocalStorage } from 'helpers/general';
 
-const usersPortfolio = getFromLocalStorage('portfolio');
+import { usePortfolio } from 'hooks/usePortfolio';
 
 const Portfolio = () => {
-  const { coinsData, findCoins } = useLcwCoinsData();
-  const [portfolioData, setPortfolioData] = useState(usersPortfolio || null);
-  const [portfolioCoinsList, setPortfolioCoinsList] = useState(null);
-  const [portfolioCurPageCoinsList, setPortfolioCurPageCoinsList] =
-    useState(null);
-  const [portfolioSummary, setPortfolioSummary] = useState({ totalValue: 0 });
-  const { currentPage, lastPage, handlePageChange } = usePages();
+  const {
+    handleSetPortfolioData,
+    findPortfolioCoins,
+    portfolioCurPageCoinsList,
+    portfolioSummary,
+    currentPage,
+    lastPage,
+    handlePageChange,
+  } = usePortfolio();
   const { isOpen, handleOpenModal, handleCloseModal } = useModal(false);
   const tableRef = useRef(null);
-  const perPageLimit = PER_PAGE_LIMIT_DEFAULT;
-
-  const handleSetPortfolioData = (coin) => {
-    setPortfolioData((prevState) => [...prevState, coin]);
-  };
-
-  const findPortfolioCoins = (queryString) => {
-    // Filters matching coins from coinsData so there are no coins that are already in portfolio -> use won't be able to add same coin more then once
-    return findCoins(queryString).filter(
-      (coin) => !portfolioData.find(({ code }) => code === coin.code)
-    );
-  };
-
-  useEffect(() => {
-    saveToLocalStorage('portfolio', portfolioData);
-  }, [portfolioData]);
-
-  useEffect(() => {
-    if (!coinsData) return;
-
-    const portfolioCoinsList = portfolioData
-      .map(({ code: portfolioCoinCode, quantity }) => {
-        const portfolioCoin = coinsData.find(
-          (coin) => coin.code === portfolioCoinCode
-        );
-        // In case no coins with specified code were found
-        if (!portfolioCoin) return undefined;
-
-        const { code, name, rate, webp64, delta } = portfolioCoin;
-        const value = rate * quantity;
-        return { code, name, rate, webp64, delta, quantity, value };
-      })
-      // In case no coins with specified code were found
-      .filter((item) => item !== undefined);
-
-    setPortfolioCoinsList(portfolioCoinsList);
-
-    const totalValue = portfolioCoinsList.reduce(
-      (acc, coin) => acc + coin.quantity * coin.rate,
-      0
-    );
-    setPortfolioSummary((prevState) => ({ ...prevState, totalValue }));
-  }, [coinsData, portfolioData]);
-
-  useEffect(() => {
-    if (!portfolioCoinsList) return;
-
-    const pageStartIndex = currentPage * perPageLimit;
-    const pageEndIndex = (currentPage + 1) * perPageLimit;
-
-    const curPageCoinsList = portfolioCoinsList.slice(
-      pageStartIndex,
-      pageEndIndex
-    );
-
-    setPortfolioCurPageCoinsList(curPageCoinsList);
-  }, [portfolioCoinsList, currentPage, perPageLimit]);
 
   return (
     <Wrapper>
