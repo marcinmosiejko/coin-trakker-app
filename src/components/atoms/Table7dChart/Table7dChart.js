ca11579c3f4cc9128dab1504c8f48ff80bb6e806
Marcin <marcinmosiejko@gmail.com>
2022-10-27 00:18:29 +0200

    Delete unnecessary code

--- src/components/atoms/Table7dChart/Table7dChart.js
+++ src/components/atoms/Table7dChart/Table7dChart.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { Wrapper } from './Table7dChart.styles';
-// import { useLcwCoinsData } from 'hooks/useLcwCoinsData';
 import { getTableChartConfig } from 'helpers/chartConfig';
 import { Line } from 'react-chartjs-2';
 import {
@@ -15,7 +14,6 @@ import {
 ChartJS.register(LineElement, PointElement, LinearScale, Title, CategoryScale);
 
 const Table7dChart = ({ chartDataset }) => {
-  // const { history7dCoinsList } = useLcwCoinsData();
   const [chartData, setChartData] = useState(null);
   const [chartOptions, setChartOptions] = useState(null);
 
