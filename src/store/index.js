f4bf63a47075de12d4f1c906747e34ba13408463
Marcin <marcinmosiejko@gmail.com>
2022-11-07 19:01:40 +0100

    Delete unnecessary code @portfolioCoinsListSlice

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { portfolioRawDataSlice } from './portfolioRawDataSlice';
 import { coinsDataSlice } from './coinsDataSlice';
-// import { portfolioCoinsListSlice } from './portfolioCoinsListSlice';
 
 export const store = configureStore({
   reducer: {
     coinsData: coinsDataSlice.reducer,
     portfolioRawData: portfolioRawDataSlice.reducer,
-    // portfolioCoinsList: portfolioCoinsListSlice.reducer,
   },
 });
