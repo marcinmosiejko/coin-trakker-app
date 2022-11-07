7c60dd5a8c37e17318288980f0fba0e3afac7862
Marcin <marcinmosiejko@gmail.com>
2022-11-07 19:15:38 +0100

    Fix eslint warning: defined but never used

--- src/views/Portfolio/Portfolio.js
+++ src/views/Portfolio/Portfolio.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import { Wrapper, ButtonWrapper } from './Portfolio.styles';
 import Table from 'components/organisms/Table/Table';
 import PortfolioSummary from 'components/molecules/PortfolioSummary/PortfolioSummary';
