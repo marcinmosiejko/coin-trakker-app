1f17c323d1e624537bf65d6794a7cb3bb7147eb3
Marcin <marcinmosiejko@gmail.com>
2022-11-10 14:34:28 +0100

    Add ShowWatchlist tests & PropTypes

--- src/components/atoms/ShowWatchlist/ShowWatchlist.js
+++ src/components/atoms/ShowWatchlist/ShowWatchlist.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { StyledButton } from './ShowWatchlist.styles';
 import WatchlistIcon from '../WatchlistIcon/WatchlistIcon';
 
 const ShowWatchlist = ({ showWatchlist, ...props }) => {
   return (
     <StyledButton {...props}>
-      <WatchlistIcon isYellow={showWatchlist} />
+      <WatchlistIcon isYellow={showWatchlist} data-testid="show" />
       <span>Watchlist</span>
     </StyledButton>
   );
+};
+
+ShowWatchlist.propTypes = {
+  showWatchlist: PropTypes.bool,
 };
 
 export default ShowWatchlist;
---
+++ src/components/atoms/ShowWatchlist/ShowWatchlist.test.js
@@ -0,0 +1,24 @@
+import { render, screen } from 'test-utils';
+import ShowWatchlist from './ShowWatchlist';
+import { theme } from 'assets/styles/theme';
+
+describe('RankAndWatch', () => {
+  it('Renders the component', () => {
+    render(<ShowWatchlist />);
+    screen.getByText('Watchlist');
+  });
+
+  it('Renders grey star when showWatchlist is false', () => {
+    render(<ShowWatchlist />);
+    expect(screen.getByTestId('show')).toHaveStyle(
+      `fill: ${theme.colors.secondary}`
+    );
+  });
+
+  it('Renders yellow star when showWatchlist is true', () => {
+    render(<ShowWatchlist showWatchlist />);
+    expect(screen.getByTestId('show')).toHaveStyle(
+      `fill: ${theme.colors.yellow}`
+    );
+  });
+});
