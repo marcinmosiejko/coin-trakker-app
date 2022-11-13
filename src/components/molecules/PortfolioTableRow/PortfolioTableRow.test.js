9baa61a3149da5c4b3e1d98d8b80908724abac08
Marcin <marcinmosiejko@gmail.com>
2022-11-13 21:18:40 +0100

    Add PortfolioTableRow tests & PropTypes

---
+++ src/components/molecules/PortfolioTableRow/PortfolioTableRow.test.js
@@ -0,0 +1,37 @@
+import { render, screen, fireEvent } from 'test-utils';
+import { portfolioCurPageCoinsList } from 'testData/portfolioCurPageCoinsList';
+import {
+  RoundSmallValue,
+  roundLargeValue,
+  convertToPercentage,
+} from 'helpers/general';
+import PortfolioTableRow from './PortfolioTableRow';
+
+describe('PortfolioTableRow', () => {
+  it('Renders the component', () => {
+    render(
+      <table>
+        <PortfolioTableRow data={portfolioCurPageCoinsList.at(0)} />
+      </table>
+    );
+  });
+
+  it("Renders component's content", () => {
+    portfolioCurPageCoinsList.forEach((coin) => {
+      const { code, name, value, quantity, rate, share } = coin;
+      render(
+        <table>
+          <PortfolioTableRow data={coin} />
+        </table>
+      );
+      screen.getByText(code);
+      screen.getByText(name);
+      screen.getByText(`$${RoundSmallValue(rate)}`);
+      screen.getByText(`$${roundLargeValue(value)}`);
+      screen.getByText(RoundSmallValue(quantity));
+      screen.getByText(`${convertToPercentage(share)}%`);
+      screen.getAllByTestId('editIcon');
+      screen.getAllByTestId('deleteIcon');
+    });
+  });
+});
